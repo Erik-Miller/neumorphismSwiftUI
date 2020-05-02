92f705640184fc8524e043f3ee9ac7f0d3139a1c
Erik Miller <eamiller34@gmail.com>
2020-05-02 16:36:59 -0700

    Second Commit

---
+++ NeumorphicSwiftUI/NeumorphicSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeumorphicSwiftUI
+//
+//  Created by Erik Miller on 5/2/20.
+//  Copyright © 2020 Erik Miller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
